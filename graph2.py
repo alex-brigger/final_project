b64f3b1e8fa5d88cd6bcc4974375ce403010d900
Alex Brigger <briggerap@arizona.edu>
2022-11-29 15:16:29 -0700

    graph made but comes as two

--- graph2.py
+++ graph2.py
@@ -2,4 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-df = pd.read_csv("generation_data.csv")
+
+def frame2():
+    df = pd.read_csv("generation_data.csv")
+    df21 = df.query("YEAR == 2021")
+    return df21.groupby(['STATE']).agg({'GENERATION (Megawatthours)': sum}).drop('US-TOTAL').sort_values(by='GENERATION (Megawatthours)', ascending=False).head(5)
+
+def plot2(frame2):
+    plt.xlabel('States')
+    plt.title('Top 5 Energy Producing States in 2021')
+    plt.ylabel('Megawatt Hours Produced')
+    frame2.plot.bar()
+
+
+def main():
+    graph_frame2 = frame2()
+    plot2(graph_frame2)
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
