8968e45da1231e8aef8ed026918c211499bb6c64
Alex Brigger <briggerap@arizona.edu>
2022-11-29 15:26:22 -0700

    format2

--- graph3.py
+++ graph3.py
@@ -3,3 +3,18 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 df = pd.read_csv("generation_data.csv")
+
+def frame3():
+    pass
+
+
+def plot3():
+    pass
+
+
+def main():
+    graph_frame3 = frame3()
+    plot3(graph_frame3)
+    plt.show()
+if __name__ == "__main__":
+    main()
