a70a93535be98f29d125e6f8745dc1906b920124
Alex Brigger <briggerap@arizona.edu>
2022-11-29 15:06:13 -0700

    add graph 1 code and data csv

---
+++ graph1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+df = pd.read_csv("generation_data.csv")
+
+def frame1():
+    num_types = df.value_counts('ENERGY SOURCE')
+    num_types = num_types.drop('Total')
+    return num_types
+
+def plot1(frame):
+    plt.xlabel('Energy Types')
+    plt.title('Total Uses of Energy Sources from 2001-2022')
+    plt.ylabel('Number of Occurences')
+    frame.plot.bar()
+
+
+def main():
+    graph_frame = frame1()
+    plot1(graph_frame)
+    
+if __name__ == "__main__":
+    main()
---
+++ graph3.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+df = pd.read_csv("generation_data.csv")
