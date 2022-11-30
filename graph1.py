adad016190abee63760a25a8430618d67c0d8e1e
Alex Brigger <briggerap@arizona.edu>
2022-11-29 20:12:17 -0700

    figure1 done

--- graph1.py
+++ graph1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 df = pd.read_csv("generation_data.csv")
 
@@ -10,10 +11,12 @@ def frame1():
     return num_types
 
 def plot1(frame):
+    frame.plot.bar(color = "Yellow")
     plt.xlabel('Energy Types')
     plt.title('Total Uses of Each Energy Source from 2001-2022')
     plt.ylabel('Number of Occurences')
-    frame.plot.bar()
+    plt.yticks(np.arange(0, 100000, step=20000))
+    plt.tight_layout()
 
 
 def main():
--- graph2.py
+++ graph2.py
@@ -5,15 +5,16 @@ from datetime import datetime
 
 def frame2():
     df = pd.read_csv("generation_data.csv")
-    df21 = df.query("YEAR == 2021")
+    df21 = df.query("YEAR == 2021" or "YEAR == 2010" or "YEAR == 2011")
     return df21.groupby(['STATE']).agg({'GENERATION (Megawatthours)': sum}).drop('US-TOTAL').sort_values(by='GENERATION (Megawatthours)', ascending=False).head(5)
 
 def plot2(frame2):
+    frame2.plot.bar()
     plt.xlabel('States')
     plt.title('Top 5 Energy Producing States in 2021')
     plt.ylabel('Megawatt Hours Produced')
-    frame2.plot.bar()
-
+    plt.legend("Energy Generated in Megawatt Hours")
+    
 
 def main():
     graph_frame2 = frame2()
