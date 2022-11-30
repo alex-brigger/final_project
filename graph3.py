e91c1fc6f07c808183771c105e3727c5d77cd489
Alex Brigger <briggerap@arizona.edu>
2022-11-29 19:36:18 -0700

    graphs work

--- graph3.py
+++ graph3.py
@@ -1,20 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 df = pd.read_csv("generation_data.csv")
 
 def frame3():
-    pass
+    graph3 = df.loc[:,["YEAR", "ENERGY SOURCE", "GENERATION (Megawatthours)"]]
+    ff = ["Coal", "Natural Gas", "Petroleum"]
+    rslt_df = graph3.loc[df['ENERGY SOURCE'].isin(ff)]
+    result = rslt_df.groupby(['YEAR']).agg({'GENERATION (Megawatthours)': sum}).drop(2022)
+    return result
 
 
 def plot3():
-    pass
+    years = np.array(range(2001,2022))
+    frame_run = frame3()
+    production = np.array(frame_run["GENERATION (Megawatthours)"])
+    plt.scatter(years, production, label = 'Total Generation Per Year')
+    plt.ylabel(("Fossil Fuel Usage"),fontsize = 20)
+    plt.xlabel("Year")
+    plt.title("Total Power Production Of Fossil Fuels Each Year")
+    a, b = np.polyfit(years, production, 1)
+    plt.plot(years, a*years+b)        
+    plt.show()
 
 
 def main():
-    graph_frame3 = frame3()
-    plot3(graph_frame3)
+    plot3()
     plt.show()
 if __name__ == "__main__":
     main()
